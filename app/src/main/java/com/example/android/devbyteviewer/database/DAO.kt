296c5e34c00e62df3a62ded8ee4ac64f3c2d7866
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2020-12-23 23:49:57 +0100

    Database and network

---
+++ app/src/main/java/com/example/android/devbyteviewer/database/DAO.kt
@@ -0,0 +1,18 @@
+package com.example.android.devbyteviewer.database
+
+import android.provider.MediaStore
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DAO {
+
+    @Query("SELECT * FROM databaseVideo")
+    suspend fun getVideos(): LiveData<List<databaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(video: databaseVideo)
+}
